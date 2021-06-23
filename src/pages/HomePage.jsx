81d64922427a0cad9fe58e2b3b941fb5c2cd3b4f
Ham Ham <ham@Hamui-MacBookPro.local>
2021-06-23 22:10:48 +0900

    print banner img

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Top3Petition from "../Component/Top3Petition"
-
-
+import Top3Petition from "../Component/Top3Petition";
+import bannerImg from "./banner.png";
 
 class HomePage extends React.Component {
-    render() {
-        return (
-            <div className="home_display">
-                <img className="home_banner" src = './banner.png' alt="배너입니다." />
-                <div className="home_top3">
-                    <Top3Petition/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="home_display">
+        <img className="home_banner" src={bannerImg} alt="배너입니다." />
+        <div className="home_top3">
+          <Top3Petition />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
