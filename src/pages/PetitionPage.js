49b3ba4395b9b7600a329fb5cba3f73283b9f379
banna117 <banna111222@gmail.com>
2021-06-28 16:04:49 +0900

    filter options added

--- src/pages/PetitionPage.js
+++ src/pages/PetitionPage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PetitionPage.css";
 import ListedPetition from "../components/ListedPetition";
+
 const petitions = [
     {
         id : 'minsoo1',
@@ -19,26 +20,65 @@ const petitions = [
         title : "학교 설비를 바꿔주세요.",
         content : "Do you agree?",
         date: "2021.06.13"
+    },
+    {
+        id : 'minsoo4',
+        title : "학교 설비를 바꿔주세요.",
+        content : "Do you agree?",
+        date: "2021.06.13"
+    },
+    {
+        id : 'minsoo5',
+        title : "학교 설비를 바꿔주세요.",
+        content : "Do you agree?",
+        date: "2021.06.13"
+    },
+    {
+        id : 'minsoo6',
+        title : "학교 설비를 바꿔주세요.",
+        content : "Do you agree?",
+        date: "2021.06.13"
+    },
+    {
+        id : 'minsoo7',
+        title : "학교 설비를 바꿔주세요.",
+        content : "Do you agree?",
+        date: "2021.06.13"
     }
   ]
 export default function PetitionPage() {
+  
+
+  const [count, setCount] = useState(0);
+
+
   return (
     <div className="petition">
       <div className="input">
         <button className="filter">필터</button>
-        <input className="filter_input" type="text" />
+        <input className="filter_input" type="text" list='filter-example' placeholder="단어를 입력하세요."/>
+
+        <datalist id = "filter-example">
+          <option value="설비"></option>
+          <option value="물품"></option>
+          <option value="학업"></option>
+        </datalist>
+
         <button className="write">작성하기</button>
       </div>
       <div className="petitionlist">
-        
-      {petitions.map((petition) => {
-          return (
-            <ListedPetition
-                key = {petition.id}
-              title={petition.title}
-              date = {petition.date}
+         {/* to show only 4 items per menu */}
+         
+        {petitions.map((petition) => {
+          
+            return (
+              <ListedPetition
+                key={petition.id}
+                title={petition.title}
+                date={petition.date}
 
-            />);
+              />);
+          
         })}
       </div>
     </div>
